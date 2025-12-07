ba397b82c858460a7847011c2b975421e5f4a94f
Gavina200761 <gavina200761@students.reinhardt.edu>
2025-12-07 12:17:29 -0500

    I made the final touches to my website, completing the wellness page in the process and correcting my footer.

--- js/script.js
+++ js/script.js
@@ -23,6 +23,20 @@ const deadlines = {
         { term: "Spring", date: "May 3 2026" }
     ]
 };
+//facts that will be randomly selected.
+const facts = [
+    "Study in shorter, focused sessions, and review notes daily instead of cramming.",
+    "College is the time to try new subjects, hobbies, and leadership roles. Expand your network.",
+    "Reinhardt offers free tutoring for all subjects.",
+    "It’s easy to join too many clubs or activities—focus on a few that matter most.",
+    "Get sunlight early in the day to improve your sleep cycle.",
+    "Professors expect students to come with questions—this builds relationships and clarifies tough material.",
+    "Break large tasks into smaller steps and reward yourself for progress.",
+    "Track expenses, avoid unnecessary debt, and use student discounts.",
+    "Many scholarships are available beyond freshman year. Keep applying annually!",
+    "Lack of rest hurts memory and focus more than most students realize.",
+    "Focusing only on academics without building relationships can make college harder emotionally.",
+];
 
 // Time difference calculation (days/hours/min/sec)
 function timeRemaining(dateString) {
@@ -82,7 +96,7 @@ function startCountdown(type) {
     }, 1000);
 }
 
-// Button event listeners
+// Button event listeners for the time left.
 document.getElementById("registerBtn").addEventListener("click", () => {
     startCountdown("register");
 });
@@ -94,3 +108,30 @@ document.getElementById("withdrawBtn").addEventListener("click", () => {
 document.getElementById("endBtn").addEventListener("click", () => {
     startCountdown("end");
 });
+// Button event listener for the random fact.
+const factBtn = document.getElementById("factBtn");
+const factText = document.getElementById("factText");
+
+if (factBtn && factText) {
+    factBtn.addEventListener("click", function () {
+        const random = Math.floor(Math.random() * facts.length);
+        factText.textContent = facts[random];
+    });
+}
+// Button event listener for random motivational quote.
+const quoteBtn = document.getElementById("quoteBtn");
+const quoteText = document.getElementById("quoteText");
+
+if (quoteBtn && quoteText) {
+    quoteBtn.addEventListener("click", async () => {
+        quoteText.textContent = "Loading...";
+
+        try {
+            const res = await fetch("https://api.quotable.io/random");
+            const data = await res.json();
+            quoteText.textContent = `"${data.content}" — ${data.author}`;
+        } catch (error) {
+            quoteText.textContent = "Could not load a quote. Try again!";
+        }
+    });
+}
